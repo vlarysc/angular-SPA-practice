7a0e266185098de8cfc2fd1bdc9ce004d553a80f
vlarysc <netosc2010@gmail.com>
2021-04-28 08:28:23 -0300

    Projeto Top!

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { SearchComponent } from './search/search.component';
+import { TableComponent } from './table/table.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, SearchComponent, TableComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  id: number;
+  email: string;
+  telefone: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    id: 1,
+    name: 'Manoel Francisco',
+    email: 'manoel@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 2,
+    name: 'Renata Ribeiros',
+    email: 'renata@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 3,
+    name: 'André Rodrigues',
+    email: 'andre@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 4,
+    name: 'Arthur Silva',
+    email: 'arthur@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 5,
+    name: 'Diego Borinato',
+    email: 'diego@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 6,
+    name: 'Regina Dantas',
+    email: 'regina@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 7,
+    name: 'Charles Baroldo',
+    email: 'charles@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+  {
+    id: 8,
+    name: 'Felipe Atilas',
+    email: 'felipe@gmail.com',
+    telefone: '(00) 0 0000-0000',
+  },
+];
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'email', 'telefone'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
